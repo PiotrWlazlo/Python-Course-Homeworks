fcbbc4a6fb2913ee20c3cbbaf85bdb1ce0c513f1
Piotr Wlazło <37460800+PiotrWlazlo@users.noreply.github.com>
2019-01-25 12:40:52 +0100

    Update EdgeColoring.py

--- Final_exam/EdgeColoring.py
+++ Final_exam/EdgeColoring.py
@@ -1,28 +1,26 @@
-from Edge import *
-from GraphFactory import *
-from Graph import *
-import math
-
+from Edges import *
+from Factory import *
+from Graphs import *
 
 class EdgeColor:
-
+    
     def __init__(self, graph):
         """The algorithm initialization."""
         if graph.is_directed():
             raise ValueError("the graph is directed")
         self.graph = graph
         self.color = dict()
-        self.m = 0  # graph.e() is slow
+        self.m = 0   # graph.e() is slow
         for edge in self.graph.iteredges():
-            if edge.source == edge.target:
+            if edge.source == edge.target: 
                 raise ValueError("a loop detected")
             else:
-                self.color[(edge.source, edge.target)] = None  # edge.source < edge.target
+                self.color[(edge.source, edge.target)] = None   # edge.source < edge.target
                 self.m += 1
         if len(self.color) < self.m:
             raise ValueError("edges are not unique")
 
-    def run(self):
+    def run_odd(self):
         length = len(list(self.graph.iternodes()))
         peripheral_edges = []
         for i in range(length):  #
@@ -51,13 +49,35 @@ class EdgeColor:
                     self.color[(node2, node1)] = self.color[edge]
                 else:
                     continue
-
+    
+    def run_even(self):
+        edges = self.graph.del_node(self.graph.v()-1)
+        #print(edges)
+        self.run_odd()
+        self.graph.add_node(self.graph.v())
+        for e in edges:
+            self.graph.add_edge(e)
+        colors = set(self.color.values())
+        colors.remove(None)
+        for i in range(self.graph.v()-1):
+            s = set()
+            for j in range(self.graph.v()-1):
+                if i==j:
+                    continue
+                elif i>j:
+                    #print("wartość",i," ",j)
+                    s.add(self.color[(j,i)])
+                else:
+                    s.add(self.color[(i,j)])
+            self.color[(i,self.graph.v()-1)] = (colors-s).pop()
+                    
+        
 
 if __name__ == '__main__':
-    N = 15
+    N = 1000
     G = GraphFactory(Graph)
     g1 = G.make_complete(N, directed=False)
     g1.save("graf.txt")
     c1 = EdgeColor(g1)
-    c1.run()
+    c1.run_even()
     print(c1.color)
